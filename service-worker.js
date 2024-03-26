53e312b6c89198c2bf4b8bb014d4516971e0d598
Stuart Dickie <dickie.stuart@gmail.com>
2024-03-26 10:12:16 -0400

    move index.html for github pages support

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 const CACHE_NAME = 'photo-booth-v1';
 const CACHE = [
-    '/html/index.html',
-    '/public/images/android-chrome-192x192.png',
-    '/public/images/android-chrome-512x512.png',
-    '/public/images/apple-touch-icon.png',
-    '/public/images/favicon-32x32.png',
-    '/public/images/favicon-16x16.png',
-    '/public/styles/normalize.css',
-    '/public/styles/global.css',
-    '/public/scripts/media-capture.js',
-    '/public/scripts/global.js'
+    'index.html',
+    'public/images/android-chrome-192x192.png',
+    'public/images/android-chrome-512x512.png',
+    'public/images/apple-touch-icon.png',
+    'public/images/favicon-32x32.png',
+    'public/images/favicon-16x16.png',
+    'public/styles/normalize.css',
+    'public/styles/global.css',
+    'public/scripts/media-capture.js',
+    'public/scripts/global.js'
 ];
 
 self.addEventListener('install', function (event) {
